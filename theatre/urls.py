2d40d6043e72a67fdaa4bf0aef4bb340703cc162
Oleksii Kostenikov <akotenikov@gmail.com>
2025-09-15 23:52:18 +0300

    added basic serializers & views & routes
    added serializers & views & routes for such models: Actor, Genre, Play

---
+++ theatre/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path, include
+from rest_framework import routers
+
+from theatre.views import ActorViewSet, GenreViewSet, PlayViewSet
+
+app_name = "theatre"
+
+router = routers.DefaultRouter()
+
+router.register("actors", ActorViewSet)
+router.register("genres", GenreViewSet)
+router.register("plays", PlayViewSet)
+
+urlpatterns = [path("", include(router.urls))]
