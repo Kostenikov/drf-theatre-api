405eaeac3ee634c98b3dd66dc13447f254e721b8
Oleksii Kostenikov <akotenikov@gmail.com>
2025-09-15 23:05:21 +0300

    added models & admin for actors, genres, plays

--- theatre/admin.py
+++ theatre/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from theatre.models import Actor, Genre, Play
+
+
+@admin.register(Actor)
+class ActorAdmin(admin.ModelAdmin):
+    list_display = ("first_name", "last_name")
+
+
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+
+
+@admin.register(Play)
+class PlayAdmin(admin.ModelAdmin):
+    list_display = ("title", "description")
--- theatre/models.py
+++ theatre/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+
+class Actor(models.Model):
+    first_name = models.CharField(max_length=63)
+    last_name = models.CharField(max_length=63)
+
+    def __str__(self) -> str:
+        return f"{self.first_name} {self.last_name}"
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=63, unique=True)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Play(models.Model):
+    title = models.CharField(max_length=128)
+    description = models.TextField(blank=True, null=True)
+    actors = models.ManyToManyField(Actor, related_name="plays")
+    genres = models.ManyToManyField(Genre, related_name="plays")
+
+    def __str__(self) -> str:
+        return self.title
