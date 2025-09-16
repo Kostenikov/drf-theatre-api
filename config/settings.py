5c29beee1889346de23bc309c7f6154fbb5df4fb
Oleksii Kostenikov <akotenikov@gmail.com>
2025-09-16 22:11:59 +0300

    Added JWT token support, changed permissions on endpoints
    Implemented JWT token support.
    Added endpoints for obtain & refresh tokens.
    Changed global authentication params to JWTAuthentication.
    Changed global permissions param to IsAuthenticated.
    Register endpoint have AllowAny permission
    Changed token's lifetime to 7 days for developing.

--- config/settings.py
+++ config/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 import os
+from datetime import timedelta
 from pathlib import Path
 from dotenv import load_dotenv
 
@@ -46,6 +47,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     # third party
     "rest_framework",
+    "rest_framework_simplejwt",
     "debug_toolbar",
     # internal
     "accounts",
@@ -136,3 +138,18 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 AUTH_USER_MODEL = "accounts.User"
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ],
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.IsAuthenticated",
+    ],
+}
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=7),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
+    "ROTATE_REFRESH_TOKENS": False,
+}
